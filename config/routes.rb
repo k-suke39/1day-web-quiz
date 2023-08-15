f4002da6e6dc34b572f6d70a3af47539e7565ba9
k-suke39 <pz.mozarla@gmail.com>
2023-08-15 22:58:45 +0900

    ⚡️

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users only: [:new, :create]
-  resources :user_sessions only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+  resources :user_sessions, only: [:new, :create, :destroy]
   root "staticpages#top"
 end
