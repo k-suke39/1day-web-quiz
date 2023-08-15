f2e855f12d163014bb5c041ebc8a9460d8debbb4
k-suke39 <pz.mozarla@gmail.com>
2023-08-15 22:51:20 +0900

    ⚡️

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
+  get 'users/new'
+  get 'users/create'
   root "staticpages#top"
 end
