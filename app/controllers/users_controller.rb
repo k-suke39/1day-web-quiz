63cffdf4ed9ad5edf6f85ec33b5c5134e0cf06bc
k-suke39 <pz.mozarla@gmail.com>
2023-08-15 23:19:10 +0900

    ⚡️

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    binding.pry
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path   
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
+
+
+
