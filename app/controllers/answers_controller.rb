3ba8bc4ed3e3449c14251d2a5f68a3cf24be148a
k-suke39 <pz.mozarla@gmail.com>
2023-08-16 09:48:16 +0900

    ⚡️

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
   def index
+
   end
 
   def show
@@ -12,17 +13,18 @@ class AnswersController < ApplicationController
 
   def create
     @answer = Answer.new(answer_params)
+    @answer.user_id = current_user.id
     if @answer.save
-      redirect_to questions_new_path, notice: t('user_sessions.create.success')
+      redirect_to answers_path, notice: t('user_sessions.create.success')
     else
       flash.now[:warning] = t('user_sessions.create.failure')
-      render :new,  status: :unprocessable_entity
+      render 'questions/new',  status: :unprocessable_entity
     end
   end
 
   private
 
   def answer_params
-    params.require(:answer).permit(:content).merge(:question_id)
+    params.require(:answer).permit(:content, :question_id)
   end
 end
