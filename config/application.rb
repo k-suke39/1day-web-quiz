d2261b0a2f7911b213df7fac1b9a7e11a85ae6cd
k-suke39 <pz.mozarla@gmail.com>
2023-08-16 17:11:26 +0900

    ⚡️

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module Web1dayQuiz
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
   end
 end
